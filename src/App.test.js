b716677e0fbd1280d886ee78d3c9d4e7a71e6449
Cindy April Leochico <dakisleochico@gmail.com>
2025-04-07 15:44:11 -0600

    fix App.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Cindy Leochico/i);
+  const linkElement = screen.getByText(/This is my Jenkins App/i);
   expect(linkElement).toBeInTheDocument();
 });
